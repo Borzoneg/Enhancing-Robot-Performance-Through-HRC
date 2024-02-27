fb0f7f6612510bca2b40c382cf8ca80bb5b91a26
Guglielmo Borzone <borzoneg@gmail.com>
2024-02-27 14:56:11 +0100

    Remove built ros nodes

--- ros_nodes/build/enhancing_robot_performance_through_hrc/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/gu/.local/share/ov/pkg/isaac_sim-2023.1.1/Enhancing-Robot-Performance-Through-HRC/ros_nodes/install/enhancing_robot_performance_through_hrc'
